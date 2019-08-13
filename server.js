87e229c48c15b88920a4919da7f1e456eb268d02
qifei_Dev <lqf1024@antbim.com>
2019-08-13 20:39:56 +0800

    code

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+// const httpProxy = require('http-proxy');
+const server = express()
+server.use(express.static(__dirname +'/dist'))
+server.listen(8888, (err) => {
+  if (err) throw err
+  console.log('> Ready on http://localhost:8888')
+})
---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react'
+class App extends React.Component{
+   constructor(props){
+     super(props)
+   }
+   render(){
+     return <div>app</div>
+   }
+ }
+
+export default App
